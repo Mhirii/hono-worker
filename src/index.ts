5cbf437be9a415f90c5766dd556d511dd15bf8a9
Mhirii <ahmedmhiri218@gmail.com>
2023-12-18 02:48:13 +0100

    signup and login

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,59 @@
-import { Hono } from 'hono'
+import { createClient } from "@supabase/supabase-js";
+import { Context, Hono } from "hono";
+import { env } from "hono/adapter";
+import { Login } from "./handleLogin";
+import { AuthDto } from "./types/authDto";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get("/", (c) => c.text("Hello Hono!"));
 
-export default app
+app.post("/login", async (c) => {
+	const { SUPABASE_URL } = env<{ SUPABASE_URL: string }>(c);
+	const { SUPABASE_KEY } = env<{ SUPABASE_KEY: string }>(c);
+	const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+	const body: AuthDto = await c.req.json();
+
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: body.email,
+		password: body.password,
+	});
+
+	if (error) {
+		return new Response(JSON.stringify(error), {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
+
+	return new Response(JSON.stringify(data), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+});
+
+app.post("/signup", async (c) => {
+	const { SUPABASE_URL } = env<{ SUPABASE_URL: string }>(c);
+	const { SUPABASE_KEY } = env<{ SUPABASE_KEY: string }>(c);
+	const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+	const body: AuthDto = await c.req.json();
+
+	const { data, error } = await supabase.auth.signUp({
+		email: body.email,
+		password: body.password,
+	});
+
+	if (error) throw error;
+
+	return new Response(JSON.stringify(data), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+});
+
+export default app;
