41ca91e16d908d10025d86dbc719f23ca5091fd8
Mhirii <ahmedmhiri218@gmail.com>
2023-12-24 07:02:12 +0100

    get tasks inside of a board

--- src/tasks.ts
+++ src/tasks.ts
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { env } from "hono/adapter";
 
 import { Hono } from "hono";
+import { boardDto } from "./types/boardDto";
+import { taskDto } from "./types/taskDto";
 
 const app = new Hono();
 
@@ -12,6 +14,7 @@ type request_body = {
 };
 
 app.get("/", (c) => c.text("Hello Hono!"));
+
 app.post("/", async (c) => {
 	const { SUPABASE_URL } = env<{ SUPABASE_URL: string }>(c);
 	const { SUPABASE_KEY } = env<{ SUPABASE_KEY: string }>(c);
@@ -54,6 +57,38 @@ app.post("/", async (c) => {
 			"Access-Control-Allow-Origin": "*",
 		},
 	});
+});
+
+app.get("/board/:id", async (c) => {
+	const { SUPABASE_URL } = env<{ SUPABASE_URL: string }>(c);
+	const { SUPABASE_KEY } = env<{ SUPABASE_KEY: string }>(c);
+	const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+	const board_id = c.req.param("id");
+	const { data, error } = await supabase
+		.from("boards")
+		.select()
+		.eq("id", board_id);
+
+	if (error) {
+		return handle_error(error);
+	}
+
+	const get_task_by_id = async (id: number) => {
+		return await supabase.from("tasks").select().eq("id", id);
+	};
+
+	const tasks = [];
+	const task_ids: number[] = data[0].tasks;
+	for (const task_id of task_ids) {
+		const { data: task_data, error: task_error } =
+			await get_task_by_id(task_id);
+		if (task_error) {
+			return handle_error(task_error);
+		}
+		tasks.push(task_data);
+	}
+	return new Response(JSON.stringify(tasks));
 });
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation> //TODO: remove explicit any
