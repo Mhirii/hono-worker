60dae037d8c0c826985cc3bd2616da0ab2cf171c
Mhirii <ahmedmhiri218@gmail.com>
2023-12-24 10:37:00 +0100

    added methods

--- src/workspaces.ts
+++ src/workspaces.ts
@@ -1,27 +1,101 @@
-import { createClient } from "@supabase/supabase-js";
-import { env } from "hono/adapter";
-
 import { Hono } from "hono";
+import { workspaceDto } from "./types/workspaceDto";
+import { getSupabaseClient, handle_error, headers } from "./utils";
 
 const app = new Hono();
 
+/* ──────────────────────── creates a new workspace ──────────────────────── */
 app.post("/", async (c) => {
-	const { SUPABASE_URL } = env<{ SUPABASE_URL: string }>(c);
-	const { SUPABASE_KEY } = env<{ SUPABASE_KEY: string }>(c);
-	const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
+	const supabase = getSupabaseClient(c);
 	const body = await c.req.json();
-
-	const { data, error } = await supabase.from("workspaces").insert({
+	const new_workspace: workspaceDto = {
 		title: body.title,
 		owned_by: body.id,
-	});
+		isSharable: true,
+	};
 
+	const { data, error } = await supabase
+		.from("workspaces")
+		.insert(new_workspace)
+		.select();
+
+	if (error) {
+		return handle_error(error);
+	}
 	return new Response(JSON.stringify(data), {
-		headers: {
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-		},
+		headers: headers,
+	});
+});
+
+/* ───────────── Retrieves workspaces owned by a specific user ───────────── */
+app.get("/user/:id", async (c) => {
+	const supabase = getSupabaseClient(c);
+
+	const user_id = c.req.param("id");
+	const { data, error } = await supabase
+		.from("workspaces")
+		.select()
+		.eq("owned_by", user_id);
+
+	if (error) {
+		return handle_error(error);
+	}
+	return new Response(JSON.stringify(data), {
+		headers: headers,
+	});
+});
+
+/* ────────────────── Retrieves a workspace by id ───────────────── */
+app.get("/:id", async (c) => {
+	const supabase = getSupabaseClient(c);
+
+	const { data, error } = await supabase
+		.from("workspaces")
+		.select()
+		.eq("id", c.req.param("id"));
+
+	if (error) {
+		return handle_error(error);
+	}
+	return new Response(JSON.stringify(data), {
+		headers: headers,
+	});
+});
+
+/* ──────── Retrieves boards associated with the specified workspace ─────── */
+app.get("/boards/:workspace_id", async (c) => {
+	const supabase = getSupabaseClient(c);
+
+	const { data, error } = await supabase
+		.from("boards")
+		.select()
+		.eq("workspace", c.req.param("workspace_id"));
+
+	if (error) {
+		return handle_error(error);
+	}
+	return new Response(JSON.stringify(data), {
+		headers: headers,
+	});
+});
+
+// TODO: Retrieve shared workspaces
+
+/* ─────────────────────────── Deletes a workspace ────────────────────────── */
+app.delete("/:id", async (c) => {
+	const supabase = getSupabaseClient(c);
+
+	const { data, error } = await supabase
+		.from("workspaces")
+		.delete()
+		.eq("id", c.req.param("id"))
+		.select();
+
+	if (error) {
+		return handle_error(error);
+	}
+	return new Response(JSON.stringify(data), {
+		headers: headers,
 	});
 });
 
