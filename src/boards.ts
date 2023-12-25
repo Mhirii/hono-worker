3423f7b65fc7d809739c5d3c31b1a07a68fe2fcc
Mhirii <ahmedmhiri218@gmail.com>
2023-12-25 19:03:13 +0100

    added helper functions

--- src/boards.ts
+++ src/boards.ts
@@ -1,8 +1,11 @@
-import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { PostgrestError, PostgrestSingleResponse, SupabaseClient } from "@supabase/supabase-js";
 import { Hono } from "hono";
 import { get_task_by_id } from "./tasks";
 import { boardDto } from "./types/boardDto";
+import { taskDto } from "./types/taskDto";
+import { workspaceDto } from "./types/workspaceDto";
 import { getSupabaseClient, handle_error, headers } from "./utils";
+import { addBoardToWorkspace } from "./workspaces";
 
 const app = new Hono();
 
@@ -90,5 +93,91 @@ app.delete("/:id", async (c) => {
 		headers: headers,
 	});
 });
+
+/**
+ * Create a new board with the given title in the specified workspace.
+ *
+ * @param {SupabaseClient} supabase - The Supabase client instance.
+ * @param {string} title - The title of the board.
+ * @param {number} workspace_id - The ID of the workspace where the board will be created.
+ * @return {Promise<{ board: boardDto | undefined; workspace: workspaceDto | undefined; error: PostgrestError | unknown | undefined }>} - A promise that resolves to an object containing the created board, workspace, and error (if any).
+ */
+export const createBoard = async (supabase: SupabaseClient, title: string, workspace_id: number): Promise<{ board: boardDto | undefined; workspace: workspaceDto | undefined; error: PostgrestError | unknown | undefined }> => {
+	// create the board
+	const { data, error } = await supabase
+		.from("boards")
+		.insert({
+			title: title,
+			workspace: workspace_id
+		})
+		.select();
+	if (error) {
+		console.log(error);
+		return { board: undefined, workspace: undefined, error };
+	}
+	const board: boardDto = data[0]
+
+	// add the board to its workspace
+	const { workspace, error: workspaceError } = await addBoardToWorkspace(supabase, workspace_id, board.id);
+	if (workspaceError) {
+		console.log(workspaceError);
+		return { board: undefined, workspace: undefined, error: workspaceError };
+	}
+	if (workspace) {
+		return { board, workspace, error: undefined };
+	}
+	return { board: undefined, workspace: undefined, error: Error("workspace does not exist") };
+};
+
+
+/**
+ * Retrieves a board from the database by its ID.
+ *
+ * @param {SupabaseClient} supabase - The Supabase client used to connect to the database.
+ * @param {number} board_id - The ID of the board to retrieve.
+ * @return {Promise<{ board: boardDto | undefined; error: PostgrestError | unknown | undefined }>} - A promise that resolves to an object containing the retrieved board and any potential error.
+ */
+export const getBoardById = async (supabase: SupabaseClient, board_id: number): Promise<{ board: boardDto | undefined; error: PostgrestError | unknown | undefined }> => {
+	const { data, error } = await supabase
+		.from("boards")
+		.select()
+		.eq("id", board_id)
+	if (error) {
+		return { board: undefined, error }
+	}
+	const board: boardDto = data[0]
+	return { board, error: undefined };
+}
+
+/**
+ * Adds a task to a board.
+ *
+ * @param {SupabaseClient} supabase - The Supabase client.
+ * @param {number} task_id - The ID of the task to be added.
+ * @param {number} board_id - The ID of the board where the task will be added.
+ * @return {Promise<{ board: boardDto | undefined; error: PostgrestError | unknown | undefined }>} - A promise that resolves to an object with the updated board and any error that occurred.
+ */
+export const addTaskToBoard = async (supabase: SupabaseClient, task_id: number, board_id: number): Promise<{ board: boardDto | undefined; error: PostgrestError | unknown | undefined }> => {
+	const { board, error } = await getBoardById(supabase, board_id)
+	if (error) {
+		return { board: undefined, error }
+	}
+	if (board) {
+		const tasks = board.tasks ? board.tasks.concat(task_id) : [task_id];
+		const { data, error } = await supabase
+			.from("boards")
+			.update({
+				tasks: tasks
+			})
+			.eq("id", board_id)
+			.select()
+		if (error) {
+			return { board: undefined, error }
+		}
+		const updatedBoard: boardDto = data[0]
+		return { board: updatedBoard, error: undefined };
+	}
+	return { board: undefined, error: Error("board does not exist") }
+}
 
 export default app;
